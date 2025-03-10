746618c22f0614ac73725aa51848fad3980207ea
Michael Wu <michaelwu@twitter.com>
2025-03-10 18:05:07 -0400

    changla

--- cmd/go-to-protobuf/main.go
+++ cmd/go-to-protobuf/main.go
@@ -22,8 +22,8 @@ import (
 	goflag "flag"
 	"fmt"
 
+	"github.com/michaelwu317/code-generator/cmd/go-to-protobuf/protobuf"
 	flag "github.com/spf13/pflag"
-	"k8s.io/code-generator/cmd/go-to-protobuf/protobuf"
 	"k8s.io/klog/v2"
 )
 
