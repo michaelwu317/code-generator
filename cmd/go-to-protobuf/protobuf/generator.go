80bb78b22d22460faec94afa5b7787ca421f475a
Michael Wu <michaelwu@twitter.com>
2025-03-10 21:07:24 -0400

    added camel casing

--- cmd/go-to-protobuf/protobuf/generator.go
+++ cmd/go-to-protobuf/protobuf/generator.go
@@ -24,6 +24,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"unicode"
 
 	"k8s.io/gengo/v2"
 	"k8s.io/gengo/v2/generator"
@@ -647,6 +648,8 @@ func membersToFields(locator ProtobufLocator, t *types.Type, localPackage types.
 			continue
 		}
 
+		field.Name = camelToSnake(m.Name)
+
 		if err := protobufTagToField(protobufTag, &field, m, t, localPackage); err != nil {
 			return nil, err
 		}
@@ -718,6 +721,21 @@ func membersToFields(locator ProtobufLocator, t *types.Type, localPackage types.
 		byTag[field.Tag] = field
 	}
 	return fields, nil
+}
+
+func camelToSnake(s string) string {
+	var result strings.Builder
+	for i, r := range s {
+		if unicode.IsUpper(r) {
+			if i > 0 {
+				result.WriteRune('_')
+			}
+			result.WriteRune(unicode.ToLower(r))
+		} else {
+			result.WriteRune(r)
+		}
+	}
+	return result.String()
 }
 
 func genComment(out io.Writer, lines []string, indent string) {
